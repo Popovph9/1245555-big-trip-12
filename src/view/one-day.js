573e422e6c00504dab54ef400d92778209e786b2
philipp <popovph9@gmil.com>
2020-08-01 10:41:33 +0300

    выполнено задание 5 личного проекта: время разбивать камни

---
+++ src/view/one-day.js
@@ -0,0 +1,9 @@
+export const createOneDayTemplate = () => {
+  return (`<ul class="trip-days">
+  <li class="trip-days__item  day">
+    <div class="day__info"></div>
+
+    <ul class="trip-events__list"></ul>
+  </li>
+</ul>`);
+};
